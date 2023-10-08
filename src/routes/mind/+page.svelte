d2d0f6b608f4bb7e6816ece0f9013a0d65154fe9
Hussein Ashraf <hphantomass@gmail.com>
2023-10-08 04:44:23 +0300

    raccoon

--- src/routes/mind/+page.svelte
+++ src/routes/mind/+page.svelte
@@ -6,7 +6,7 @@
   //import { MindARThree } from "$libs/Mind-Ar/image-target";
   import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
   import { onMount } from "svelte";
-  import { LoadGLTF } from "$lib";
+  import { LoadGLTF, InteractionManager } from "$lib";
   // import ARnftThreejs from "@webarkit/arnft-threejs";
 
   onMount(async () => {
@@ -24,21 +24,42 @@
       transparent: true,
       opacity: 0.5,
     });
+    const interactionManager = new InteractionManager(
+      renderer,
+      camera,
+      renderer.domElement
+    );
     scene.add(light);
     const plane = new THREE.Mesh(geometry, material);
     //anchor.group.add(plane);
 
-    const gltf = await LoadGLTF("examples/Data/models/flamingo/Flamingo.glb");
+    const gltf = await LoadGLTF("examples/Data/models/raccoon/scene.gltf");
     console.log(gltf);
-    gltf.scene.rotateX(Math.PI / 2);
-    gltf.scene.rotateY(Math.PI * 1.5);
+    // gltf.scene.rotateX(Math.PI / 2);
+    // gltf.scene.rotateY(Math.PI * 1.5);
     gltf.scene.scale.set(0.1, 0.1, 0.1);
-    gltf.scene.position.set(0, 0, -30);
+    gltf.scene.position.set(0, -0.4, 0);
+    //gltf.scene.position.set(0, 0, -30);
     anchor.group.add(gltf.scene);
+    let clock = new THREE.Clock();
+    const model = gltf.scene.children[0];
 
+    const animation = gltf.animations[0];
+    const mixer = new THREE.AnimationMixer(model);
+    const action = mixer.clipAction(animation);
+    console.log(model);
+    model.addEventListener("click", () => {
+      console.log("Cliked On COin");
+      // Cliked = true;
+      // setTimeout(() => {
+      //   Cliked = false;
+      // }, 2000);
+    });
+    action.play();
     const start = async () => {
       await mindarThree.start();
       renderer.setAnimationLoop(() => {
+        mixer.update(clock.getDelta());
         renderer.render(scene, camera);
       });
     };
