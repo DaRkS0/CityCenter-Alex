4aa92e06362f6ddc803257350d2a386e5c785ed7
Hussein Ashraf <hphantomass@gmail.com>
2023-10-10 00:10:59 +0300

    form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,52 @@
   <title>City Center Alex | 2023</title>
 </svelte:head>
 
-<main class="h-full w-full" />
-<!-- 
-<style>
-  main {
-    background-image: url(img/BG.webp);
-    background-size: 100% 100%;
-    /* background-size: cover; */
-    background-repeat: no-repeat;
-  }
-</style> -->
+<main class="h-full w-full flex flex-col items-center justify-center">
+  <form action="" class="w-full">
+    <div class="mb-3 w-[70%] mx-auto">
+      <img class="max-w-[40%] mb-3" src="img/name.webp" alt="" />
+      <div class="w-full relative mx-auto">
+        <img class="max-w-full" src="img/inputbg.webp" alt="" />
+        <input
+          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none"
+          required
+          type="text"
+        />
+      </div>
+    </div>
+
+    <div class="mb-3 w-[70%] mx-auto">
+      <img class="max-w-[60%] mb-3" src="img/mobile.webp" alt="" />
+      <div class="w-full relative mx-auto">
+        <img class="max-w-full" src="img/inputbg.webp" alt="" />
+        <input
+          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none"
+          inputmode="tel"
+          required
+          type="text"
+        />
+      </div>
+    </div>
+
+    <div class="mb-3 w-[70%] mx-auto">
+      <img class="max-w-[60%] mb-3" src="img/email.webp" alt="" />
+      <div class="w-full relative mx-auto">
+        <img class="max-w-full" src="img/inputbg.webp" alt="" />
+        <input
+          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none"
+          required
+          type="email"
+        />
+      </div>
+    </div>
+
+    <div class="mb-3 w-[70%] mx-auto text-center">
+      <button class="relative">
+        <img class="max-w-full" src="img/button.webp" alt="" />
+        <div class="absolute inset-0 w-full h-full flex items-center">
+          <img class="m-auto max-h-[70%]" src="img/submit.webp" alt="" />
+        </div>
+      </button>
+    </div>
+  </form>
+</main>
