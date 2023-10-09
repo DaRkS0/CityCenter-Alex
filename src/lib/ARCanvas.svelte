367813269455e60ebb5ddad93dfc26479e8d85bf
Hussein Ashraf <hphantomass@gmail.com>
2023-10-09 10:49:05 +0300

    Wrapper

---
+++ src/lib/ARCanvas.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import * as THREE from "three";
+  import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+  import { onMount } from "svelte";
+  import { LoadGLTF, InteractionManager, CreateMixer } from "$lib";
+  import type { AnchorMarker } from "$lib";
+
+  export let imageTargetSrc = "";
+  export let maxTrack = 1;
+  export let anchors: AnchorMarker[];
+  let container: HTMLDivElement;
+  let mixers: THREE.AnimationMixer[] = [];
+  let clock = new THREE.Clock();
+
+  onMount(async () => {
+    const mindarThree = new MindARThree({
+      container,
+      imageTargetSrc,
+      maxTrack,
+    });
+    const { renderer, scene, camera } = mindarThree;
+
+    let directionalLight = new THREE.DirectionalLight("#fff", 0.6);
+    directionalLight.position.set(0.5, 0, 0.866);
+    scene.add(directionalLight);
+
+    const interactionManager = new InteractionManager(
+      renderer,
+      camera,
+      container
+    );
+    clock = new THREE.Clock();
+
+    anchors.forEach(async (a, id) => {
+      const anchor = mindarThree.addAnchor(id);
+      const gltf = await LoadGLTF(a.path);
+
+      if (a.onload !== undefined) a.onload(gltf);
+
+      if (a.animated) {
+        mixers.push(await CreateMixer(gltf));
+      }
+
+      const model = gltf.scene.children[0];
+      interactionManager.add(model);
+      model.addEventListener("click", () => {
+        if (a.onclick !== undefined) a.onclick();
+      });
+      anchor.group.add(gltf.scene);
+    });
+
+    const start = async () => {
+      await mindarThree.start();
+      renderer.setAnimationLoop(() => {
+        const Delta = clock.getDelta();
+        mixers.forEach((mixer) => mixer.update(Delta));
+        renderer.render(scene, camera);
+      });
+    };
+    start();
+  });
+</script>
+
+<div
+  id="container"
+  bind:this={container}
+  class="flex flex-col items-stretch justify-stretch relative w-full h-full overflow-hidden"
+/>
+
+<style>
+  :global(video) {
+    inset: 0 !important;
+  }
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,13 @@
+import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
+
 // place files you want to import through the `$lib` alias in this folder.
 export { InteractionManager as InteractionManager } from "$lib/Interactive";
 export { LoadGLTF as LoadGLTF } from "$lib/utils";
 export { CreateMixer as CreateMixer } from "$lib/utils";
 export { ColorGUIHelper as ColorGUIHelper } from "$lib/utils";
+export type AnchorMarker = {
+  animated: boolean;
+  path: string;
+  onload?: (gltf: GLTF) => void | undefined;
+  onclick?: () => void | undefined;
+};
