1935182f89c66890f164b5f584c47edb71134543
Hussein Ashraf <hphantomass@gmail.com>
2023-10-10 00:35:18 +0300

    url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,39 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { NewDoc } from "$lib/firebase/database/client";
+
+  let name = "";
+  let email = "";
+  let phone = "";
+  const re = new RegExp("^01[0125][0-9]{8}$");
+  $: validPhone = re.test(phone);
+  async function SaveUser() {
+    if (!validPhone) {
+      alert("Invalid phone number");
+      return;
+    }
+    const info = await NewDoc("Invitees", {
+      name,
+      email,
+      phone,
+    });
+    goto("game?uid=" + info.id);
+  }
+</script>
+
 <svelte:head>
   <title>City Center Alex | 2023</title>
 </svelte:head>
 
 <main class="h-full w-full flex flex-col items-center justify-center">
-  <form action="" class="w-full">
+  <form class="w-full" on:submit={SaveUser}>
     <div class="mb-3 w-[70%] mx-auto">
       <img class="max-w-[40%] mb-3" src="img/name.webp" alt="" />
       <div class="w-full relative mx-auto">
         <img class="max-w-full" src="img/inputbg.webp" alt="" />
         <input
-          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none"
+          bind:value={name}
+          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none font-lg font-semibold font-mono"
           required
           type="text"
         />
@@ -21,7 +45,8 @@
       <div class="w-full relative mx-auto">
         <img class="max-w-full" src="img/inputbg.webp" alt="" />
         <input
-          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none"
+          bind:value={phone}
+          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none font-lg font-semibold font-mono"
           inputmode="tel"
           required
           type="text"
@@ -34,7 +59,8 @@
       <div class="w-full relative mx-auto">
         <img class="max-w-full" src="img/inputbg.webp" alt="" />
         <input
-          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none"
+          bind:value={email}
+          class="absolute inset-0 z-10 py-[3%] px-[7%] overflow-hidden w-full bg-transparent outline-none font-lg font-semibold font-mono"
           required
           type="email"
         />
