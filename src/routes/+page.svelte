8db8bfa8f84a915243e35ba457b98a660700ac3a
Hussein Ashraf <hphantomass@gmail.com>
2023-10-09 22:21:14 +0300

    firebase

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <title>City Center Alex | 2023</title>
+</svelte:head>
+
+<main class="h-full w-full" />
+
+<style>
+  main {
+    background-image: url(img/BG.webp);
+    background-size: 100% 100%;
+    /* background-size: cover; */
+    background-repeat: no-repeat;
+  }
+</style>
