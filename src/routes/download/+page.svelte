193950400db05d07a78c228609356608dd00b008
Hussein Ashraf <hphantomass@gmail.com>
2023-10-13 16:46:54 +0300

    Download

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,50 +1,41 @@
 <script>
-	// @ts-nocheck
+  // @ts-nocheck
 
-	import { GetDocs } from '$lib/firebase/database/client';
-	import CsvHandler from '$lib/CSVHandler.svelte';
-	let data;
-	async function Loading() {
-		const Docs = await GetDocs('Invitees');
-		data = Docs.docs.map((q) => ({
-			Name: q.data().Name,
-			Project: q.data().Project,
-			Unit: q.data().Unit,
-			Mobile: q.data().Mobile,
-			Invitees: q.data().Invitees,
-			Scaned: q.data().Scaned
-		}));
+  import { GetDocs } from "$lib/firebase/database/client";
+  import CsvHandler from "$lib/CSVHandler.svelte";
+  let data;
+  async function Loading() {
+    const Docs = await GetDocs("Invitees");
+    data = Docs.docs.map((q) => ({
+      Name: q.data().name,
+      Mobile: q.data().phone,
+      Email: q.data().email,
+      Score: q.data().score ?? 0,
+    }));
 
-		let tota = data.reduce((accum, item) => accum + item.Scaned, 0);
-		//data = Docs.docs.map((q) => ({ ...q.data() }));
-		data.push({
-			Name: 'Total',
-			Mobile: undefined,
-			Classification: undefined,
-			Region: undefined,
-			Scaned: data.length
-		});
-		data.push({
-			Name: 'Total Inside',
-			Mobile: undefined,
-			Classification: undefined,
-			Region: undefined,
-			Scaned: tota
-		});
-	}
+    data.push({
+      Name: "Total",
+      Mobile: undefined,
+      Email: undefined,
+      Score: data.length,
+    });
+  }
 </script>
 
-<div id="wrapper" class=" h-full flex flex-col items-center justify-center px-[2%] pt-8 pb-4">
-	{#await Loading()}
-		<p class="text-2xl font-bold my-auto">Loading..</p>
-	{:then}
-		<div class="mx-auto">
-			<CsvHandler {data} filename="Event Members">
-				<button
-					class="disabled:bg-gray-600 px-5 py-2 mt-4 font-semibold text-lg disabled:text-white bg-orange-400 rounded-md"
-					>Download</button
-				>
-			</CsvHandler>
-		</div>
-	{/await}
+<div
+  id="wrapper"
+  class=" h-full flex flex-col items-center justify-center px-[2%] pt-8 pb-4"
+>
+  {#await Loading()}
+    <p class="text-2xl font-bold my-auto">Loading..</p>
+  {:then}
+    <div class="mx-auto">
+      <CsvHandler {data} filename="Event Members">
+        <button
+          class="disabled:bg-gray-600 px-5 py-2 mt-4 font-semibold text-lg disabled:text-white bg-orange-400 rounded-md"
+          >Download</button
+        >
+      </CsvHandler>
+    </div>
+  {/await}
 </div>
